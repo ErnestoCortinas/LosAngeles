20695ea4ec31c9b212b07f4e3b2fe9829cd9de02
Ernesto Cortinas <ernestocortinas@hotmail.com>
2023-11-06 11:44:40 -0600

    Primera carga de guardado del proyecto

---
+++ prueba.py
@@ -0,0 +1,151 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory, current_app
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.date import DateTrigger
+from datetime import datetime
+import pytz
+from flask_sse import sse
+from apscheduler.jobstores.base import JobLookupError
+from flask import redirect, request
+from flask import render_template_string
+
+app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost"  # Asegúrate de que Redis esté en ejecución y configurado
+
+utc = pytz.utc
+
+now = datetime.now(utc)
+
+UPLOAD_FOLDER = "pdf_manager/uploads"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define una lista para almacenar los datos de los recordatorios
+recordatorios_programados = []
+
+# Función para enviar correos
+def enviar_correo(asunto, destinatario, cuerpo):
+    smtp_server = 'smtp.live.com'  # Cambia a tu servidor SMTP
+    smtp_port = 587  # Cambia al puerto de tu servidor SMTP
+    smtp_user = 'ernestocortinas@hotmail.com'  # Cambia a tu usuario SMTP
+    smtp_password = 'neto129'  # Cambia a tu contraseña SMTP
+
+    msg = MIMEMultipart()
+    msg['From'] = smtp_user
+    msg['To'] = destinatario
+    msg['Subject'] = asunto
+
+    msg.attach(MIMEText(cuerpo, 'plain'))
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_user, smtp_password)
+
+    text = msg.as_string()
+    server.sendmail(smtp_user, destinatario, text)
+
+    server.quit()
+
+# Definir la función recordatorio
+def recordatorio(asunto, segundos_hasta_alarma, destinatario, app):
+    with app.app_context():
+        mensaje = f"¡Es hora de tu recordatorio! Asunto: {asunto}. Faltan {segundos_hasta_alarma} segundos para la alarma."
+        current_app.extensions['sse'].publish({"message": mensaje}, type="notification")
+
+        # Envía la alerta por correo
+        enviar_correo(asunto, destinatario, mensaje)
+
+@app.route('/')
+def index():
+    return render_template('subir.html')
+
+scheduler = BackgroundScheduler()
+scheduler.start()  # Inicia el planificador
+
+@app.route('/subir', methods=['GET', 'POST'])
+def subir_archivo():
+    if request.method == 'POST':
+        archivo = request.files['archivo']
+        seccion = request.form['seccion']
+        if archivo:
+            carpeta_seccion = os.path.join(app.config['UPLOAD_FOLDER'], seccion)
+            if not os.path.exists(carpeta_seccion):
+                os.makedirs(carpeta_seccion)
+
+            archivo.save(os.path.join(carpeta_seccion, archivo.filename))
+            return redirect(url_for('subir_archivo'))
+
+    return render_template('subir.html')
+
+@app.route('/ver/<seccion>')
+def ver_archivos(seccion):
+    archivos = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], seccion))
+    return render_template('ver_archivos.html', seccion=seccion, archivos=archivos)
+
+@app.route('/descargar/<seccion>/<archivo>')
+def descargar(seccion, archivo):
+    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], seccion), archivo)
+
+@app.route('/programar-recordatorio', methods=['POST'])
+def programar_recordatorio():
+    asunto = request.form['asunto']
+    destinatario = request.form['destinatario']
+    fecha = request.form['fecha']
+    hora = request.form['hora']
+    fecha_hora_recordatorio = f"{fecha} {hora}"
+    fecha_hora_recordatorio = datetime.strptime(fecha_hora_recordatorio, "%Y-%m-%d %H:%M")
+
+    segundos_hasta_alarma = int((fecha_hora_recordatorio - datetime.now()).total_seconds())
+
+    # Almacena los datos en la lista de recordatorios programados
+    recordatorios_programados.append({
+        'asunto': asunto,
+        'segundos_hasta_alarma': segundos_hasta_alarma,
+        'destinatario': destinatario,
+    })
+
+    # Programa el recordatorio y envía la alerta por correo
+    scheduler.add_job(
+        lambda: recordatorio(asunto, segundos_hasta_alarma, destinatario),
+        DateTrigger(run_date=fecha_hora_recordatorio)
+    )
+
+    # Define el regreso a la página anterior en 1.5 segundos
+    return render_template_string("""
+    <html>
+    <head>
+    <meta http-equiv="refresh" content="1.5;url={{ referrer }}" />
+    </head>
+    <body>
+    Recordatorio programado con éxito. Redirigiendo...
+    </body>
+    </html>
+    """, referrer=request.referrer)
+
+@app.route('/ver-recordatorios')
+def ver_recordatorios():
+    return render_template('ver_recordatorios.html', recordatorios=recordatorios_programados)
+
+@app.route('/eliminar-recordatorio/<job_id>')
+def eliminar_recordatorio(job_id):
+    try:
+        scheduler.remove_job(job_id)
+        return "Recordatorio eliminado con éxito"
+    except JobLookupError:
+        return "El recordatorio no se pudo encontrar o ya ha sido eliminado"
+
+@app.route('/nuevo-recordatorio')
+def nuevo_recordatorio():
+    return render_template('programar_recordatorio.html')
+
+@app.route('/mostrar-recordatorios')
+def mostrar_recordatorios():
+    recordatorios = scheduler.get_jobs()
+    return render_template('ver_recordatorios.html', recordatorios=recordatorios)
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.run(debug=True)
